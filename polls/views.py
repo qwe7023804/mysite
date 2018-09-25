3e57eb3c771700ba669b62b99bf206d317ca3d86
qwe7023804 <1720933154@qq.com>
2018-09-25 10:27:19 +0800

    Revert "Revert "登录更新""
    This reverts commit 8c7dc80ea72f09e98e10732239cc60c214a4b1e6.

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello, Yang')
+    return render(request, 'index.html')
+
+def login_action(request):
+    if request.method == 'GET':
+        username = request.GET.get('username')
+        password = request.GET.get('password')
+        if username =='' or password == '':
+            return render(request, 'index.html', {'error': 'username or password is none'})
