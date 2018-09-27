fb1daf624278c6b66195c6d80cf01bd867359905
qwe7023804 <1720933154@qq.com>
2018-09-27 11:57:58 +0800

    完善登录

--- test_platform/urls.py
+++ test_platform/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from user_app import views
 
 
 urlpatterns = {
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('index/', views.index),
-    path("login_action/", views.login_action),
+    path('login_action/', views.login_action),
+    path('home/', views.home),
+    path('logout/', views.logout),
 }
-
--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
 
 # Create your views here.
 
@@ -10,9 +10,31 @@ def index(request):
 
 # 处理登录请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username", '')
+        password = request.POST.get("password", '')
+        #调用数据库用户名和密码进行
+        user = auth.authenticate(
+            username = username, password = password)
+        print(user)
+        if user is None:
+            return render(request, 'index.html',
+                            {'error': 'username or password error'})
+        else:
+            #return HttpResponse('success')
+            response = HttpResponseRedirect('/home/')
+            response.set_cookie('username', username, 3600)
+            return response
+    else:
+        return render(request, 'index.html',
+                        {'error': 'username or password error'})
 
-        if username == "" or password == "":
-            return render(request, "index.html", {"error": "用户名或者密码错误"})
+def home(request):
+    username = request.COOKIES.get('username', '')
+    return render(request, 'home.html', {'username': username})
+
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    #清除cookie里保存的username
+    return response
