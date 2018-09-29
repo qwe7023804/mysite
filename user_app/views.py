edc9563e44b88a7ebdca430ac50b96766337167e
qwe7023804 <1720933154@qq.com>
2018-09-29 16:17:17 +0800

    更新视图

--- user_app/views.py
+++ user_app/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.https://sobooks.cc/books/10313.html#respond
-#@login_required
 def index(request):
     return render(request, "index.html")
 
